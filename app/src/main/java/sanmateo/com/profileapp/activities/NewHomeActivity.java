a7bcef31900dc4c1745556dd358cc2ab87329390
JovsQ <jovel@ideyatech.com>
2017-09-13 14:27:07 +0800

    04: Initialized drawer

--- app/src/main/java/sanmateo/com/profileapp/activities/NewHomeActivity.java
+++ app/src/main/java/sanmateo/com/profileapp/activities/NewHomeActivity.java
@@ -1,25 +1,295 @@
 package sanmateo.com.profileapp.activities;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import butterknife.BindDimen;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import io.realm.RealmResults;
+import io.realm.Sort;
+import retrofit2.adapter.rxjava.HttpException;
 import sanmateo.com.profileapp.R;
+import sanmateo.com.profileapp.adapters.CustomNavMenuAdapter;
 import sanmateo.com.profileapp.base.BaseActivity;
+import sanmateo.com.profileapp.enums.ApiAction;
+import sanmateo.com.profileapp.fragments.ChangePasswordDialogFragment;
+import sanmateo.com.profileapp.fragments.CustomBottomSheetDialogFragment;
+import sanmateo.com.profileapp.helpers.ApiErrorHelper;
+import sanmateo.com.profileapp.helpers.ApiRequestHelper;
+import sanmateo.com.profileapp.helpers.AppConstants;
+import sanmateo.com.profileapp.helpers.LogHelper;
+import sanmateo.com.profileapp.helpers.PicassoHelper;
+import sanmateo.com.profileapp.helpers.PrefsHelper;
+import sanmateo.com.profileapp.helpers.RealmHelper;
+import sanmateo.com.profileapp.interfaces.OnApiRequestListener;
+import sanmateo.com.profileapp.interfaces.OnConfirmDialogListener;
+import sanmateo.com.profileapp.models.others.CustomMenu;
+import sanmateo.com.profileapp.models.response.Announcement;
+import sanmateo.com.profileapp.models.response.ApiError;
+import sanmateo.com.profileapp.models.response.AuthResponse;
+import sanmateo.com.profileapp.models.response.GenericMessage;
+import sanmateo.com.profileapp.models.response.News;
+import sanmateo.com.profileapp.singletons.CurrentUserSingleton;
 
 /**
  * Created by USER on 9/13/2017.
  */
 
-public class NewHomeActivity extends BaseActivity {
+public class NewHomeActivity extends BaseActivity implements OnApiRequestListener{
+
+    private static final int SELECT_IMAGE = 1;
+    private static final int CAPTURE_IMAGE = 2;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.navigationView)
+    NavigationView navigationView;
+
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.iv_profile_image)
+    ImageView iv_profile_image;
+
+    @BindView(R.id.lv_menu)
+    ListView lv_menu;
+
+    @BindView(R.id.iv_blur_background)
+    ImageView iv_blur_background;
+
+    @BindView(R.id.pb_load_image)
+    ProgressBar pb_load_image;
+
+    @BindView(R.id.tv_profile_name)
+    TextView tv_profile_name;
+
+    @BindDimen(R.dimen._90sdp)
+    int profilePicSize;
 
     private Unbinder unbinder;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private String uploadToBucket;
+    private Uri fileUri;
+    private File fileToUpload;
+    private ApiRequestHelper apiRequestHelper;
+    private String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         unbinder = ButterKnife.bind(this);
+
+        currentUserSingleton = CurrentUserSingleton.getInstance();
+        apiRequestHelper = new ApiRequestHelper(this);
+
+
+        if (currentUserSingleton.getCurrentUser() == null) {
+            showConfirmDialog("", "San Mateo Profile App", "Sorry, but your session has expired!",
+                    "Re-login", "", new OnConfirmDialogListener() {
+                        @Override
+                        public void onConfirmed(String action) {
+                            logout();
+                        }
+
+                        @Override
+                        public void onCancelled(String action) {
+
+                        }
+                    });
+        } else {
+            token = currentUserSingleton.getCurrentUser().getToken();
+            initNavigationDrawer();
+        }
+    }
+
+    public void initNavigationDrawer() {
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.open_drawer, R.string.close_drawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        initSideDrawerMenu();
+    }
+
+    private CurrentUserSingleton currentUserSingleton;
+
+    private void initSideDrawerMenu() {
+
+        iv_profile_image.setOnClickListener(view1 -> showChangeProfilePicMenu());
+
+        PicassoHelper.loadBlurImageFromURL(this, currentUserSingleton.getCurrentUser().getPicUrl(),
+                R.drawable.placeholder_image, 25, iv_blur_background);
+
+        PicassoHelper.loadImageFromURL(currentUserSingleton.getCurrentUser().getPicUrl(),
+                profilePicSize, Color.TRANSPARENT, iv_profile_image, pb_load_image);
+
+        tv_profile_name.setText(currentUserSingleton.getCurrentUser().getFirstName() + " " +
+                currentUserSingleton.getCurrentUser().getLastName());
+
+        ArrayList<CustomMenu> menuList = new ArrayList<>();
+        menuList.add(new CustomMenu(R.drawable.menu_new_send_sos, "Send SOS", true));
+        menuList.add(new CustomMenu(R.drawable.menu_new_information, "Information", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_directories, "Directories", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_announcements, "Disaster Management", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_incidents, "Incident Report", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_map, "Map", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_gallery, "Gallery", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_fb, "Social Media", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_rate, "Rate Us", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_password, "Change Password", false));
+        menuList.add(new CustomMenu(R.drawable.menu_new_logout, "Logout", false));
+
+        final CustomNavMenuAdapter adapter = new CustomNavMenuAdapter(this, menuList);
+        adapter.setCustomMenuListener(() -> setPanicContacts());
+        lv_menu.setAdapter(adapter);
+        lv_menu.setOnItemClickListener((adapterView, view, i, l) -> {
+            switch (i) {
+                case 0:
+                    sendSOS();
+                    break;
+                case 1:
+                    moveToOtherActivity(InformationActivity.class);
+                    break;
+                case 2:
+                    moveToOtherActivity(DirectoriesActivity.class);
+                    break;
+                case 3:
+                    moveToOtherActivity(DisasterManagementActivity.class);
+                    break;
+                case 4:
+                    moveToOtherActivity(IncidentsActivity.class);
+                    break;
+                case 5:
+                    moveToOtherActivity(MapActivity.class);
+                    break;
+                case 6:
+                    moveToOtherActivity(GalleryActivity.class);
+                    break;
+                case 7:
+                    if (isNetworkAvailable()) {
+                        moveToOtherActivity(SocialMediaActivity.class);
+                    } else {
+                        showToast(AppConstants.WARN_CONNECTION);
+                    }
+                    break;
+                case 8:
+                    final Uri uri = Uri.parse("market://details?id=" + getPackageName());
+                    final Intent myAppLinkToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                    try {
+                        startActivity(myAppLinkToMarket);
+                    } catch (ActivityNotFoundException e) {
+                        showToast(AppConstants.WARN_UNABLE_TO_FIND_APP);
+                    }
+                    break;
+                case 9:
+                    changePassword();
+                    break;
+                case 10:
+                    showConfirmDialog("", "Logout", "Are you sure you want to logout from the app?",
+                            "Yes", "No", new OnConfirmDialogListener() {
+                                @Override
+                                public void onConfirmed(String action) {
+                                    logout();
+                                }
+
+                                @Override
+                                public void onCancelled(String action) {
+
+                                }
+                            });
+                    break;
+            }
+        });
+    }
+
+    private void changePassword() {
+        ChangePasswordDialogFragment fragment = ChangePasswordDialogFragment.newInstance();
+        fragment.setOnChangePasswordListener((oldPassword, newPassword) ->
+                apiRequestHelper.changePassword(token, currentUserSingleton.getCurrentUser().getEmail(),
+                        oldPassword, newPassword));
+        fragment.show(getFragmentManager(), "chane password");
+    }
+
+    private void showChangeProfilePicMenu() {
+        uploadToBucket = AppConstants.BUCKET_PROFILE_PIC;
+        final CustomBottomSheetDialogFragment bottomSheetDialogFragment = CustomBottomSheetDialogFragment.newInstance();
+        bottomSheetDialogFragment.setOnChangeProfilePicListener(action -> {
+            bottomSheetDialogFragment.dismiss();
+            switch (action) {
+                case "via gallery":
+                    final Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
+                    break;
+                case "via camera":
+                    final Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    try {
+                        fileToUpload = createImageFile();
+                        fileUri = Uri.fromFile(fileToUpload);
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                        startActivityForResult(cameraIntent, CAPTURE_IMAGE);
+                    } catch (Exception ex) {
+                        showConfirmDialog("", "Capture Image",
+                                "We can't get your image. Please try again.", "Close", "", null);
+                    }
+                    break;
+            }
+        });
+        bottomSheetDialogFragment.show(getSupportFragmentManager(), "bottom sheet");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_IMAGE) {
+                fileToUpload = getFile(data.getData(), UUID.randomUUID().toString() + ".png");
+                uploadFileToAmazon();
+            } else if (requestCode == CAPTURE_IMAGE) {
+                fileToUpload = rotateBitmap(fileUri.getPath());
+                uploadFileToAmazon();
+            }
+        }
+    }
+
+    private void uploadFileToAmazon() {
+        /** delete previous profile pic from s3 if it's not the default profile pic using gravatar */
+        if (!currentUserSingleton.getCurrentUser().getPicUrl()
+                .contains("http://www.gravatar.com/avatar/")) {
+            deleteImage(AppConstants.BUCKET_ROOT, currentUserSingleton.getCurrentUser().getPicUrl());
+        }
+        uploadImageToS3(uploadToBucket, fileToUpload, 1, 1);
     }
 
     @Override
@@ -28,5 +298,108 @@ public class NewHomeActivity extends BaseActivity {
         if (unbinder != null) {
             unbinder.unbind();
         }
+    }
+
+    @Override
+    public void onApiRequestBegin(ApiAction action) {
+        if (action.equals(ApiAction.GET_NEWS)) {
+            showCustomProgress("Fetching news, Please wait...");
+        } else if (action.equals(ApiAction.PUT_CHANGE_PW)) {
+            showCustomProgress("Changing password, Please wait...");
+        } else if (action.equals(ApiAction.PUT_CHANGE_PROFILE_PIC)) {
+            showCustomProgress("Changing your profile pic, Please wait...");
+        }
+    }
+
+    @Override
+    public void onApiRequestSuccess(ApiAction action, Object result) {
+        dismissCustomProgress();
+        if (action.equals(ApiAction.GET_NEWS)) {
+//            final ArrayList<News> news = (ArrayList<News>) result;
+//            newsSingleton.getAllNews().clear();
+//            final RealmHelper<News> realmHelper = new RealmHelper<>(News.class);
+//
+//            if (!news.isEmpty()) {
+//                for (News n : news) {
+//                    realmHelper.replaceInto(n);
+//                }
+//            }
+//
+//            if (realmHelper.count() > 0) {
+//                final RealmResults<News> cachedNews = realmHelper.findAll("createdAt", Sort.DESCENDING);
+//                for (News n : cachedNews) {
+//                    newsSingleton.getAllNews().add(n);
+//                }
+//            }
+//            newsSingleton.getAllNews().addAll(news);
+        } else if (action.equals(ApiAction.GET_NEWS_BY_ID)) {
+//            final RealmHelper<News> realmHelper = new RealmHelper<>(News.class);
+//            final News news = (News) result;
+//            realmHelper.replaceInto(news);
+//            newsSingleton.getAllNews().add(0, news);
+        } else if (action.equals(ApiAction.PUT_CHANGE_PW)) {
+            final GenericMessage genericMessage = (GenericMessage) result;
+            showToast(genericMessage.getMessage());
+        } else if (action.equals(ApiAction.PUT_CHANGE_PROFILE_PIC)) {
+            final GenericMessage genericMessage = (GenericMessage) result;
+            showToast("You have successfully changed your profile pic");
+
+            /** save new profile pic url */
+            fileToUpload = null;
+            fileUri = null;
+
+            final RealmHelper<AuthResponse> realmHelper = new RealmHelper<>(AuthResponse.class);
+            realmHelper.openRealm();
+            currentUserSingleton.getCurrentUser().setPicUrl(genericMessage.getMessage());
+            realmHelper.update(currentUserSingleton.getCurrentUser());
+            realmHelper.commitTransaction();
+
+            LogHelper.log("changePic", "new pic url ---> " + currentUserSingleton.getCurrentUser().getPicUrl());
+
+            PicassoHelper.loadBlurImageFromURL(this, currentUserSingleton.getCurrentUser().getPicUrl(),
+                    R.drawable.placeholder_image, 25, iv_blur_background);
+
+            PicassoHelper.loadImageFromURL(currentUserSingleton.getCurrentUser().getPicUrl(),
+                    profilePicSize, Color.TRANSPARENT, iv_profile_image, pb_load_image);
+
+        }
+
+//        if (!action.equals(ApiAction.PUT_CHANGE_PW)) {
+//            rvHomeMenu.getAdapter().notifyDataSetChanged();
+//            rvHomeMenu.smoothScrollToPosition(0);
+//        }
+    }
+
+    @Override
+    public void onApiRequestFailed(ApiAction action, Throwable t) {
+        dismissCustomProgress();
+        handleApiException(t);
+        LogHelper.log("err", "error in ---> " + action + " cause ---> " + t.getMessage());
+        if (t instanceof HttpException) {
+            final ApiError apiError = ApiErrorHelper.parseError(((HttpException) t).response());
+            if (action.equals(ApiAction.PUT_CHANGE_PW)) {
+                showConfirmDialog(action.name(), "Change Password Failed", apiError.getMessage(), "Close", "", null);
+            }
+        }
+    }
+
+    private void logout() {
+        /** clear all singletons */
+//        newsSingleton.clearAll();
+//        incidentsSingleton.clearAll();
+        currentUserSingleton.setCurrentUser(null);
+
+        new RealmHelper<>(News.class).deleteRecords();
+        new RealmHelper<>(Announcement.class).deleteRecords();
+        new RealmHelper<>(AuthResponse.class).deleteRecords();
+
+        PrefsHelper.setString(NewHomeActivity.this,
+                AppConstants.PREFS_LOCAL_EMERGENCY_KITS, "");
+
+        final Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+        animateToRight(NewHomeActivity.this);
     }
 }
