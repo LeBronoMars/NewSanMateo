f45616f03408fe38dfe011144387a031c2f7bfe7
JovsQ <jvlquiamco@gmail.com>
2017-10-19 03:08:21 +0800

    06: Share on facebook

--- app/src/main/java/sanmateo/com/profileapp/activities/IncidentDetailActivity.java
+++ app/src/main/java/sanmateo/com/profileapp/activities/IncidentDetailActivity.java
@@ -1,10 +1,19 @@
 package sanmateo.com.profileapp.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 
 import java.text.ParseException;
 import java.util.Calendar;
@@ -16,6 +25,7 @@ import butterknife.OnClick;
 import butterknife.Unbinder;
 import sanmateo.com.profileapp.R;
 import sanmateo.com.profileapp.base.BaseActivity;
+import sanmateo.com.profileapp.helpers.AppConstants;
 import sanmateo.com.profileapp.models.response.Incident;
 import sanmateo.com.profileapp.singletons.PicassoSingleton;
 
@@ -26,6 +36,7 @@ import sanmateo.com.profileapp.singletons.PicassoSingleton;
 public class IncidentDetailActivity extends BaseActivity {
 
     private Unbinder unbinder;
+    private Incident incident;
 
     @BindView(R.id.ll_action_bar)
     LinearLayout llActionBar;
@@ -48,6 +59,9 @@ public class IncidentDetailActivity extends BaseActivity {
     @BindView(R.id.tv_incident_details)
     TextView tvIncidentDetails;
 
+    @BindView(R.id.ll_container)
+    LinearLayout llContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,11 +69,18 @@ public class IncidentDetailActivity extends BaseActivity {
         unbinder = ButterKnife.bind(this);
         setStatusBarColor(llActionBar, statusBar);
 
+        shareCallBackManager = CallbackManager.Factory.create();
         initDetails();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        shareCallBackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
     private void initDetails() {
-        Incident incident = getIntent().getParcelableExtra("incident");
+        incident = getIntent().getParcelableExtra("incident");
         if (!incident.getImages().isEmpty()) {
             PicassoSingleton.getInstance().getPicasso().load(incident.getImages())
                     .placeholder(R.drawable.placeholder_image)
@@ -85,9 +106,42 @@ public class IncidentDetailActivity extends BaseActivity {
         onBackPressed();
     }
 
+    private CallbackManager shareCallBackManager;
+
     @OnClick(R.id.iv_share)
     public void share() {
-        showToast("share");
+        if (isNetworkAvailable()) {
+
+            final String imageUrl = incident.getImages();
+            final ShareDialog shareDialog = new ShareDialog(this);
+            shareDialog.registerCallback(shareCallBackManager, new FacebookCallback<Sharer.Result>() {
+                @Override
+                public void onSuccess(Sharer.Result result) {
+                }
+
+                @Override
+                public void onCancel() {
+
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+                }
+            });
+
+            if (ShareDialog.canShow(ShareLinkContent.class)) {
+                final ShareLinkContent linkContent = new ShareLinkContent.Builder()
+                        .setContentTitle(incident.getIncidentType())
+                        .setImageUrl(Uri.parse(imageUrl.isEmpty() ? AppConstants.SAN_MATEO_LOGO : imageUrl))
+                        .setContentUrl(Uri.parse("www.google.com"))
+                        .setContentDescription(incident.getIncidentAddress())
+                        .build();
+                shareDialog.show(linkContent, AppConstants.IS_FACEBOOK_APP_INSTALLED
+                        ? ShareDialog.Mode.NATIVE : ShareDialog.Mode.FEED);
+            }
+        } else {
+            showSnackbar(llContainer, AppConstants.WARN_CONNECTION);
+        }
     }
 
     @Override
