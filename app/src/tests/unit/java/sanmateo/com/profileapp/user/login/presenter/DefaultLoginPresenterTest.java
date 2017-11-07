844bb4c10a494140781a594ad7f9b0ea10a1715f
Russel <>
2017-11-07 21:35:18 +0800

    Add failing test for login presenter

--- app/src/tests/unit/java/sanmateo/com/profileapp/user/login/presenter/DefaultLoginPresenterTest.java
+++ app/src/tests/unit/java/sanmateo/com/profileapp/user/login/presenter/DefaultLoginPresenterTest.java
@@ -13,6 +13,7 @@ import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Completable;
 import io.reactivex.Single;
+import sanmateo.com.profileapp.api.user.InvalidAccountException;
 import sanmateo.com.profileapp.factory.user.UserFactory;
 import sanmateo.com.profileapp.user.login.model.User;
 import sanmateo.com.profileapp.user.login.model.UserLoader;
@@ -120,6 +121,60 @@ public class DefaultLoginPresenterTest {
 
         // verify that login success was called.
         verify(view).showLoginSuccess();
+
+        // verify that we use the single scheduler
+        verify(rxSchedulerUtil).singleAsyncSchedulerTransformer();
+
+        // since we are done with our Login Request. This is to verify that we dont have
+        // any interactions with any objects related to login.
+        verifyNoMoreInteractions(rxSchedulerUtil, userLoader, view);
+    }
+
+    @Test
+    public void loginFailed() {
+        // attach our view
+        attachView();
+
+        String expectedEmail = "ned@fanders.com";
+        String expectedPassword = "P@ssw0rd";
+
+        // when LoginView#getEmail is called, it will return ned@flanders.com
+        given(view.getEmail()).willReturn(expectedEmail);
+
+        // when LoginView#getPassword is called, it will return P@ssw0rd
+        given(view.getPassword()).willReturn(expectedPassword);
+
+        // given when userLoader#login with any string value passed on its parameter, it will
+        // return an InvalidAccountException
+        given(userLoader.login(anyString(), anyString()))
+            .willReturn(Single.error(InvalidAccountException::new));
+
+        // perform actual login call
+        classUnderTest.login();
+
+        // verify that show progress was called.
+        verify(view).showProgress();
+
+        // verify that UserLoader#login was called only once and to verify if the parameters received
+        // by this method is equal to our given expected email & password using ArgumentCaptor.
+        ArgumentCaptor<String> argumentCaptorEmail = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> argumentCaptorPassword = ArgumentCaptor.forClass(String.class);
+
+        verify(userLoader, times(1)).login(argumentCaptorEmail.capture(),
+                                           argumentCaptorPassword.capture());
+
+        assertThat(argumentCaptorEmail.getValue()).isEqualTo(expectedEmail);
+        assertThat(argumentCaptorPassword.getValue()).isEqualTo(expectedPassword);
+
+        verify(view).getEmail();
+
+        verify(view).getPassword();
+
+        // verify that hide progress was called.
+        verify(view).hideProgress();
+
+        // verify that login failed was called.
+        verify(view).showLoginFailed();
 
         // verify that we use the single scheduler
         verify(rxSchedulerUtil).singleAsyncSchedulerTransformer();
