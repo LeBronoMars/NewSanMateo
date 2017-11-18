de5cdd6c9415b1bceb57e1980d8eb9795d4ccfe5
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-11-18 13:11:51 +0800

    Include WaterLevel on Database entities

--- app/src/main/java/sanmateo/com/profileapp/database/AppDatabase.java
+++ app/src/main/java/sanmateo/com/profileapp/database/AppDatabase.java
@@ -3,8 +3,10 @@ package sanmateo.com.profileapp.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+import sanmateo.com.profileapp.models.response.WaterLevel;
 import sanmateo.com.profileapp.user.local.UserDao;
 import sanmateo.com.profileapp.user.login.model.User;
+import sanmateo.com.profileapp.waterlevel.local.WaterLevelDao;
 
 /**
  * Created by rsbulanon on 17/11/2017.
@@ -12,7 +14,8 @@ import sanmateo.com.profileapp.user.login.model.User;
 
 @Database(
     entities = {
-       User.class
+       User.class,
+       WaterLevel.class
     },
     exportSchema = false,
     version = 1
@@ -20,4 +23,6 @@ import sanmateo.com.profileapp.user.login.model.User;
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract UserDao userDao();
+
+    public abstract WaterLevelDao waterLevelDao();
 }
