7fbd6582e9272bab08762f3fe93b38966ee76fee
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-11-18 11:52:03 +0800

    WIP SplashActivity

---
+++ app/src/main/java/sanmateo/com/profileapp/splash/view/SplashActivity.java
@@ -0,0 +1,49 @@
+package sanmateo.com.profileapp.splash.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.hannesdorfmann.mosby3.mvp.MvpActivity;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import sanmateo.com.profileapp.splash.presenter.SplashPresenter;
+import sanmateo.com.profileapp.user.login.view.LoginActivity;
+
+/**
+ * Created by rsbulanon on 18/11/2017.
+ */
+
+public class SplashActivity extends MvpActivity<SplashView, SplashPresenter> implements SplashView {
+
+
+    @Inject
+    SplashPresenter splashPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+
+        presenter.checkForLocalUser();
+    }
+
+    @NonNull
+    @Override
+    public SplashPresenter createPresenter() {
+        return splashPresenter;
+    }
+
+    @Override
+    public void onRedirectToHome() {
+        Log.d("app", "redirect to home");
+    }
+
+    @Override
+    public void onRedirectToLogin() {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+}
