31d4d58fd0295bb2765ac63062a4d42d841b136e
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-12-02 11:40:38 +0800

    Include News on Database entities

--- app/src/main/java/sanmateo/com/profileapp/database/AppDatabase.java
+++ app/src/main/java/sanmateo/com/profileapp/database/AppDatabase.java
@@ -3,6 +3,8 @@ package sanmateo.com.profileapp.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
+import sanmateo.com.profileapp.news.local.NewsDao;
+import sanmateo.com.profileapp.news.usecase.News;
 import sanmateo.com.profileapp.user.local.UserDao;
 import sanmateo.com.profileapp.user.login.model.User;
 import sanmateo.com.profileapp.waterlevel.local.WaterLevelDao;
@@ -14,6 +16,7 @@ import sanmateo.com.profileapp.waterlevel.usecase.WaterLevel;
 
 @Database(
     entities = {
+       News.class,
        User.class,
        WaterLevel.class
     },
@@ -21,6 +24,8 @@ import sanmateo.com.profileapp.waterlevel.usecase.WaterLevel;
     version = 2
 )
 public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract NewsDao newsDao();
 
     public abstract UserDao userDao();
 
