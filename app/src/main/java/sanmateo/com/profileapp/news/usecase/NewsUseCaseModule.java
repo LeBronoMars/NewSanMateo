060be197cece534d9a129c2ec0e9b3bec819a6ec
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-12-02 11:42:19 +0800

    Include RoomNewsModule on NewsUseCaseModule

--- app/src/main/java/sanmateo/com/profileapp/news/usecase/NewsUseCaseModule.java
+++ app/src/main/java/sanmateo/com/profileapp/news/usecase/NewsUseCaseModule.java
@@ -2,12 +2,14 @@ package sanmateo.com.profileapp.news.usecase;
 
 import dagger.Binds;
 import dagger.Module;
+import sanmateo.com.profileapp.news.local.RoomNewsModule;
 import sanmateo.com.profileapp.news.usecase.remote.NewsRemoteModule;
 
 /**
  * Created by rsbulanon on 28/11/2017.
  */
 @Module(includes = {
+                       RoomNewsModule.class,
                        NewsRemoteModule.class
 })
 public interface NewsUseCaseModule {
