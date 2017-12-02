c79cadd3049ac9d5f82e519030c3546298b25629
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-12-02 10:43:19 +0800

    Add generation of array of NewsDto on NewsFactory

--- app/src/main/java/sanmateo/com/profileapp/factory/NewsFactory.java
+++ app/src/main/java/sanmateo/com/profileapp/factory/NewsFactory.java
@@ -3,6 +3,10 @@ package sanmateo.com.profileapp.factory;
 import org.fluttercode.datafactory.impl.DataFactory;
 
 import sanmateo.com.profileapp.api.news.NewsDto;
+import sanmateo.com.profileapp.util.TimeUtils;
+
+
+import static sanmateo.com.profileapp.util.TimeUtils.toDate;
 
 /**
  * Created by rsbulanon on 02/12/2017.
@@ -16,15 +20,25 @@ public class NewsFactory {
         NewsDto newsDto = new NewsDto();
 
         newsDto.body = FACTORY.getRandomChars(100);
-        newsDto.createdAt = FACTORY.getBirthDate().toString();
-        newsDto.deletedAt = FACTORY.getBirthDate().toString();
+        newsDto.createdAt = toDate(FACTORY.getBirthDate().getTime());
+        newsDto.deletedAt = toDate(FACTORY.getBirthDate().getTime());
         newsDto.id = FACTORY.getNumberBetween(0, 1000);
         newsDto.imageUrl = FACTORY.getRandomChars(32);
         newsDto.reportedBy = FACTORY.getFirstName();
         newsDto.status = FACTORY.getRandomChars(4);
         newsDto.sourceUrl = FACTORY.getRandomChars(32);
         newsDto.title = FACTORY.getRandomWord();
-        newsDto.updatedAt = FACTORY.getBirthDate().toString();
+        newsDto.updatedAt = toDate(FACTORY.getBirthDate().getTime());
         return newsDto;
+    }
+
+    public static NewsDto[] dtos() {
+        NewsDto[] newsDtos = new NewsDto[FACTORY.getNumberBetween(1, 100)];
+
+        for (int i = 0; i < newsDtos.length; i++) {
+            newsDtos[i] = dto();
+        }
+
+        return newsDtos;
     }
 }
