bca1425991d30e98b8522b5dc67bed2cd240cefa
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-12-03 08:26:17 +0800

    Add IncidentDao

---
+++ app/src/main/java/sanmateo/com/profileapp/incident/usecase/local/IncidentDao.java
@@ -0,0 +1,36 @@
+package sanmateo.com.profileapp.incident.usecase.local;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import sanmateo.com.profileapp.incident.usecase.Incident;
+
+/**
+ * Created by rsbulanon on 02/12/2017.
+ */
+
+@Dao
+public interface IncidentDao {
+
+    @Query("SELECT COUNT(*) FROM Incident")
+    Single<Long> count();
+
+    @Delete
+    void delete(Incident incident);
+
+    @Query("SELECT * FROM Incident ORDER BY updatedAt DESC")
+    Maybe<List<Incident>> findAll();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Incident incident);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAll(List<Incident> incidents);
+}
