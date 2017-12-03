05a89958d1e8b6e79af37c4d01dfeed79d92d28d
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-12-03 18:04:59 +0800

    Create ProfileGlideModule

---
+++ app/src/main/java/sanmateo/com/profileapp/framework/ProfileGlideModule.java
@@ -0,0 +1,11 @@
+package sanmateo.com.profileapp.framework;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by rsbulanon on 03/12/2017.
+ */
+@GlideModule
+public class ProfileGlideModule extends AppGlideModule {
+}
